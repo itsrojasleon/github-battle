95d573bd763f75f7afa39013e1f789bdfd4c01dd
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-03-22 22:12:25 -0600

    right

--- src/components/popular.js
+++ src/components/popular.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-class Popular extends React.Component {
-  state = {
-    selectedLanguage: 'All'
-  }
-
-  updateLanguage = (lang) => {
-    this.setState({ selectedLanguage: lang })
-  }
-
-  render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+const SelectLanguage = (props) => {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className="languages">
         {languages.map(lang => (
-          <li style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
-            onClick={this.updateLanguage.bind(null, lang)}
+          <li style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+            onClick={props.onSelect.bind(null, lang)}
             key={lang}>
             {lang}
           </li>
         ))}
       </ul>
+    )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+
+}
+
+class Popular extends React.Component {
+  state = { selectedLanguage: 'All' }
+
+  updateLanguage = lang => {
+    this.setState({ selectedLanguage: lang })
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     )
   }
 }
