c97395c53a12ff9d1a0692b97438a0dc16b4cd62
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-03-29 19:36:12 -0600

    refactor app.js and player-preview.js

--- src/components/player-preview.js
+++ src/components/player-preview.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const PlayerPreview = (props) => {
+const PlayerPreview = ({ avatar, username, children }) => {
   return (
     <div>
       <div className="column players">
         <img
           className="avatar"
-          src={props.avatar}
-          alt={props.username}
+          src={avatar}
+          alt={username}
         />
-        <h2 className="username">@{props.username}</h2>
+        <h2 className="username">@{username}</h2>
       </div>
-      {props.children}
+      {children}
     </div>
   )
 }
--- src/containers/app.js
+++ src/containers/app.js
@@ -19,9 +19,7 @@ class App extends React.Component {
             <Route exact path="/battle" component={Battle} />
             <Route path="/battle/results" component={Results} />
             <Route path="/popular" component={Popular} />
-            <Route render={() => {
-              return <p>Not found</p>
-            }} />
+            <Route render={() => <p>Not found</p>} />
           </Switch>
         </div>
       </Router>
